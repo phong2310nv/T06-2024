7f024a0868a821aa8591825b71d3eb8fb2a1fe22
tranhoandiepbp@gmail.com <thdiep@cmc.com.vn>
2024-07-30 17:05:57 +0700

    BTVN: tu 1 den 5 ngay 14

--- thdiep/Day14/script.js
+++ thdiep/Day14/script.js
@@ -6,44 +6,25 @@
 //     { name: "Nghiem 4", scores: [100, 100, 100] },
 // ];
 // // Hiển thị danh sách học sinh có điểm trung bình lớn hơn 90
-// const filteredStudents = students.filter(student => {
-//   // Tính tổng điểm số của sinh viên
-//     const totalScore = student.scores.reduce((a,b) => a + b, 0);
-
-//   // Tính điểm trung bình
-//   const averageScore = totalScore / student.scores.length;
-
-//   // Gán averageScore vào đối tượng học sinh
-//   student.averageScore = averageScore;
-//   // Lọc sinh viên có điểm trung bình lớn hơn 90
-  
-//   return averageScore > 90;
+// const arr1 = students.filter(function(std) {
+//   const average = (std.scores[0] + std.scores[1] + std.scores[2]) / std.scores.length;
+//   console.log(average.toFixed(2));
+//   return average > 90;
 // });
+// console.log(arr1);
 
-// // Sắp xếp danh sách học sinh theo điểm trung bình từ cao đến thấp
-// filteredStudents.sort((a, b) => b.averageScore - a.averageScore);
-
-// // Hiển thị danh sách học sinh có điểm trung bình lớn hơn 90
-// console.log("Danh sách học sinh có điểm trung bình lớn hơn 90:");
-// filteredStudents.forEach(student => {
-//   console.log(`Tên: ${student.name}, Điểm trung bình: ${student.averageScore.toFixed(2)}`);
-// });
 
 // // Tạo một mảng mới với các thông tin điểm mỗi môn được lưu vào từng key
-// const detailedStudents = filteredStudents.map(student => {
-//     return {
-//       name: student.name,
-//       averageScore: student.averageScore,
-//       score1: student.scores[0],
-//       score2: student.scores[1],
-//       score3: student.scores[2],
-//     };
-//   });
-// console.log(detailedStudents);
-// console.log("Danh sách học sinh có điểm trung bình lớn hơn 90 (sắp xếp từ cao đến thấp):");
-// detailedStudents.forEach(student => {
-//   console.log(`Tên: ${student.name}, Điểm trung bình: ${student.averageScore.toFixed(2)}, Điểm 1: ${student.score1}, Điểm 2: ${student.score2}, Điểm 3: ${student.score3}`);
+// const newStudents = students.map(function(std){
+//   const newObj = {
+//     name : std.name,
+//     toan : std.scores[0],
+//     ly : std.scores[1],
+//     hoa : std.scores[2],
+//   };
+//   return newObj;
 // });
+// console.log(newStudents);
 
 // Bài 2: Cho mảng products sau:
 const product = [
@@ -96,15 +77,59 @@ const product = [
       code: "BC06",
     },
   ];
-// Câu 1: Sử dụng hàm forEach, hiển thị ra màn hình thông tin các sản phẩm theo cấu trúc.
-// "Sản phẩm a, msp b: giá bán c vnd, có các màu x-y-z, được phân
-// phối bởi công ty d
-// Hàng tồn:  e"
-// Trong đó, a là tên sp, b là mã sp, c là giá bán, xyz là các màu
-// của sản phẩm, d là nhà cung cấp, e là số lượng tồn kho.
-product.forEach(product => {
-    const { name, price, amount, color, origin, code } = product;
-    console.log(
-      `Sản phẩm ${name}, msp ${code}: giá bán ${price} vnd, có các màu ${color.join("-")}, được phân phối bởi công ty ${origin}\nHàng tồn: ${amount}`
-    );
-  });
+// // Câu 1: Sử dụng hàm forEach, hiển thị ra màn hình thông tin các sản phẩm theo cấu trúc.
+// // "Sản phẩm a, msp b: giá bán c vnd, có các màu x-y-z, được phân
+// // phối bởi công ty d
+// // Hàng tồn:  e"
+// // Trong đó, a là tên sp, b là mã sp, c là giá bán, xyz là các màu
+// // của sản phẩm, d là nhà cung cấp, e là số lượng tồn kho.
+// product.forEach(product => {
+//   const { name, price, amount, color, origin, code } = product;
+//   console.log(
+//     `Sản phẩm ${name}, msp ${code}: giá bán ${price} vnd, có các màu ${color.join("-")}, được phân phối bởi công ty ${origin}\nHàng tồn: ${amount}`
+//   );
+// });
+// Câu 2: sử dụng một hàm đã học của array để tạo ra một mảng sản phẩm mới ,
+// với mức giá của sản phẩm có mã "BC05" tăng lên 2000, và sản phẩm có mã "BC04" thêm màu "purple",
+// tất cả tên của sản phẩm được viết hoa.
+// const arr3 = product.map(function (prd) {
+//   const newPrice = prd.code === "BC05" ? prd.price + 2000 : prd.price;
+//   console.log(newPrice);
+//   const newColors = prd.color;
+//   if (prd.code === "BC04"){
+//     newColors.push("purple");
+//   }
+//   console.log(newColors);
+//   const newName = prd.name.toLowerCase();
+//   const newProducts = {
+//     origin: prd.origin,
+//     code : prd.code,
+//     name : newName,
+//     price : newPrice,
+//     color : newColors
+//   }
+//   return newProducts;
+// });
+// console.log(arr3);
+
+//  Câu 3: sử dụng hàm của array để lọc ra
+// những sản phẩm có lượng hàng tồn kho nhỏ hơn 10 và có giá ít nhất 8000.
+// const arr4 = product.filter(function(prd){
+//   const newFiltterProducts = prd.amount < 10 && prd.price >= 8000;
+//     return newFiltterProducts;
+// });
+// console.log(arr4);
+// Câu 4: sử dụng hàm của array để tìm 1 ra sản phẩm
+// của nhà cung cấp "Hong ha" và ( có giá trên 10000 hoặc có số hàng tồn <6 ).
+// const arr5 = product.find(function(prd){
+//   const isValid = prd.origin === "Hong ha" && (prd.price > 10000 || prd.amount < 6);
+//   return isValid;
+// });
+// console.log(arr5);
+// Câu 5: sử dụng hàm của array để kiểm tra xem, có phải có ít nhất một
+//  sản phẩm  của nhà cung cấp "Thien long" có giá trên 10000 không
+// const arr6 = product.some(function(prd){
+//   const isValid = prd.origin === "Thien long" && prd.price >10000;
+//   return isValid;
+// });
+// console.log(arr6);
